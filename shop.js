2b7902094b23679b4972a14dc967f96a4c405019
Leopoldo Romano <leopoldoromano7@gmail.com>
2025-12-02 16:59:02 +0100

    Aggiunti i filtri per prezzo e parola, modificata la logica da quella del prof perche' a me non funzionava, c'ho sbattuto la testa ma sembra risolta.

--- shop.js
+++ shop.js
@@ -19,9 +19,18 @@ window.addEventListener('scroll', updateNavbar);
 // filtro
 let categoryWrapper = document.querySelector("#categoryWrapper");
 let cardsWrapper = document.querySelector("#cardsWrapper");
+let priceInput = document.querySelector("#priceInput");
+let priceNumber = document.querySelector("#priceNumber");
+let wordInput = document.querySelector("#wordInput");
 
 fetch("./shop.json").then( (response)=> response.json()).then( (data)=>  {
     console.log(data);  
+
+                  data.forEach(item => {
+                        item.price = +item.price.replace('€','');
+                    });
+
+
 
         function setCategoryFilter() {
             let categories = data.map(  (shop)=> shop.category  )
@@ -68,7 +77,7 @@ fetch("./shop.json").then( (response)=> response.json()).then( (data)=>  {
                                         <img src=${shop.image} class="card-img-top" alt="...">
                                         <div class="card-body">
                                             <h5 class="card-title">${shop.name}</h5>
-                                            <p class="card-subtitle"> ${shop.price}</p>
+                                            <p class="card-subtitle"> ${shop.price}€</p>
                                             <p  class="card-subtitle"> ${shop.category}</p>
 
                                             <a href="#" class="btn">Acquista</a>
@@ -108,6 +117,43 @@ fetch("./shop.json").then( (response)=> response.json()).then( (data)=>  {
             })
         })
     
+
+        function setPriceInput() {
+            let minPrice = data[0].price;
+            let maxPrice = data[0].price;
+
+            // calcola min e max scorrendo tutto l'array
+            data.forEach(item => {
+                if (+item.price > +maxPrice) maxPrice = +item.price;
+                if (+item.price < +minPrice) minPrice = +item.price;
+            });
+
+            priceInput.max = maxPrice;
+            priceInput.min = minPrice;
+            priceInput.value = maxPrice;
+            priceNumber.innerHTML = maxPrice;
+        }
+         setPriceInput();   
+
+          priceInput.addEventListener("input", ()=> {
+
+            priceNumber.innerHTML = priceInput.value;  
+            filterByPrice();
+         })
+
+         function filterByPrice() {
+            let filtered = data.filter( (annuncio)=>  +annuncio.price <= +priceInput.value )
+            showCards(filtered);
+         }
+         
+
+
+         wordInput.addEventListener( "input" ,()=> {
+             filterByWord()
+         } )
+
+
+         
 
 } ); //FINE FETCH
 
